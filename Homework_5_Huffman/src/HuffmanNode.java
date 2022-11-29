4b1a6a334218a105a1cfc70c8d346645fe66476f
Andrei281 <andreiklesse@gmail.com>
2022-11-28 21:57:51 -0800

    Added field to HuffmanNode

--- Homework_5_Huffman/src/HuffmanNode.java
+++ Homework_5_Huffman/src/HuffmanNode.java
@@ -2,11 +2,13 @@
 public class HuffmanNode {
 	private int ASCIIVal;
 	private int frequency;
+	private int location;
 	private HuffmanNode[] childrenNodes;
 	
-	HuffmanNode(Integer ASCIIVal, int frequency, HuffmanNode[] childrenNodes) {
+	HuffmanNode(Integer ASCIIVal, int frequency, int location, HuffmanNode[] childrenNodes) {
 		this.ASCIIVal = ASCIIVal; // Nullable, for nodes representing frequency sums
 		this.frequency = frequency;
+		this.location = location;
 		this.childrenNodes = childrenNodes;
 	}
 	
